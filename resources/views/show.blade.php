f4dcdc35842b8e1998d524163205521138c0f24e
LoaiYoub <luaiayoubi@windowslive.com>
2022-03-30 04:43:20 +0300

    first commit

---
+++ resources/views/show.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @if (isset($task))
+    <h1>{{$task}}</h1>
+    @endif
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('show', function () {
+    return view('show');
+});
+Route::get('tasks', function () {
+    $tasks = [
+        'first-task'=>'Tasks 1',
+        'second-task'=>'Tasks 2',
+        'third-task'=>'Tasks 3'
+    ];
+    return view('tasks', compact('tasks'));
+});
+Route::get('show/{id}', function ($id) {
+    $tasks = [
+        'first-task'=>'Tasks 1',
+        'second-task'=>'Tasks 2',
+        'third-task'=>'Tasks 3'
+    ];
+    try {
+        $task = $tasks[$id];
+        return view('show', compact('task'));
+      }catch(Exception $e) {
+        return view('tasks', compact('tasks'));
+      }
+
+
+});
